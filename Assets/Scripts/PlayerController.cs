f22367b93c8307f5e07de732db7c94776e985f99
pranavipathuri <pp42496@srcs.org>
2022-05-24 17:45:36 -0500

    car switches lanes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject main; 
     private float laneSpace = 10f;
+    private float currentLane = 0;
+    private float xPos = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,23 +27,29 @@ public class PlayerController : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.A) && gameObject.transform.position.x > -5)
         {
-            gameObject.transform.position += new Vector3(-10, 0, 0);
+            currentLane -= 10;
         }
 
         if(Input.GetKeyDown(KeyCode.D) && gameObject.transform.position.x < 5)
         {
-            gameObject.transform.position += new Vector3(10, 0, 0);
+            currentLane += 10;
+            
         }
-
+        if (xPos != currentLane)
+        {
+            xPos = Mathf.MoveTowards(xPos, currentLane, 20 * Time.deltaTime);
+            
+        }
+        gameObject.transform.position = new Vector3(xPos, 0, 0);
 
         // Move the vehicle
 
-       // horizontalInput = Input.GetAxis("Horizontal");
-       // forwardInput = Input.GetAxis("Vertical");
+        // horizontalInput = Input.GetAxis("Horizontal");
+        // forwardInput = Input.GetAxis("Vertical");
         // Moves the car forward based on vertial input
-       // transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // Rotates the car based on horizontal input
-      //  transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
-       
+        //  transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
     }
 }
